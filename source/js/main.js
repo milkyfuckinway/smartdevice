e2a7be5b2b2618c82b5e0a13e16f86f76cda89d2
milkyfuckinway <0-0_0@bk.ru>
2023-03-03 04:41:38 +0300

    fix

--- source/js/main.js
+++ source/js/main.js
@@ -1,19 +1,19 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {testFunciton} from './test';
 
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
-
   // Utils
   // ---------------------------------
 
   iosVhFix();
-
   // Modules
   // ---------------------------------
 
+  testFunciton();
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
---
+++ source/js/test.js
@@ -0,0 +1,22 @@
+const testFunciton = () => {
+  const textButton = document.querySelector('.button--transparent-blue');
+  const textContainer = document.querySelector('.about__text');
+  const displayedText = document.querySelector('.about__displayed');
+  const hiddenText = document.querySelector('.about__hidden');
+
+  const setRootStyles = () => {
+    document.documentElement.style.setProperty('--displayed-text-height', `${displayedText.offsetHeight}px`);
+    document.documentElement.style.setProperty('--hidden-text-height', `${hiddenText.offsetHeight}px`);
+    document.documentElement.style.setProperty('--full-text-height', `${displayedText.offsetHeight + hiddenText.offsetHeight}px`);
+  };
+
+  setRootStyles();
+
+  window.addEventListener('resize', setRootStyles);
+
+  textButton.addEventListener('click', () => {
+    textContainer.classList.toggle('about__text--shown');
+  });
+};
+
+export {testFunciton};
